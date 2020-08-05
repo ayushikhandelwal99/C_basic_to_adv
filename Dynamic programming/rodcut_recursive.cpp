58d59f0506e720df8381087d6066c7244907654b
Ayushi Khandelwal <ayushikhandelwal9919@yahoo.com>
2020-08-05 13:00:31 +0530

    dynamic programming

---
+++ Dynamic programming/rodcut_recursive.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<limits.h>
+
+int RodCut(int *length,int *price,int n) {
+	if(n==0)
+	return 0;
+	
+	int max=INT_MAX;
+	for(int i=1;i<=n;i++)
+	{
+		int cost=price[i-1]+RodCut(length,price,n-i);
+		
+		if(cost>max)
+		max=cost;
+	}
+		
+	return max;
+}
+
+int main() {
+	int length[]={1,2,3,4,5,6,7,8};
+	int price[]={1,5,8,9,10,17,17,20};
+	int n=4;
+	
+	printf("%d",RodCut(length,price,n));	
+	return 0;
+
+}
